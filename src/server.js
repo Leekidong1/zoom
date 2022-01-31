8e2e7bb3a0f41e9f2440b3efa042e695cdd618d5
KIDONG <dlrlehd0420@gmail.com>
2022-01-31 18:53:17 +0900

    20220131_zoom_project

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,5 +9,19 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app); // http서버
+const wss = new WebSocket.Server({ server }); // Websocket 서버
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconected from Client"));
+    socket.on("message", (message) => {
+        console.log(message.toString('utf-8'));
+    });
+    socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);
